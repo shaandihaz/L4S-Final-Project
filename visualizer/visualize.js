3df0ecd4b0ecd0bcceca857fe123de5e36dc7ed4
miasantomauro <mia_santomauro@brown.edu>
2020-05-02 20:00:03 -0400

    adds a basic visualization

--- visualizer/visualize.js
+++ visualizer/visualize.js
@@ -1,19 +1,106 @@
+let numActors;
+let numProps;
+let numScenes = 1; // default to 1 so the initial error handling of the next button works
+let sceneData;
+
+let currScene = 0;
 
 function parse(raw_json) {
 	const json = JSON.parse(raw_json);
-	const numActors = json["numActors"];
-	const numProps = json["numProps"];
-	const numScenes = json["numScenes"];
-	const sceneData = json["sceneData"];
-	console.log("number of actors: " + numActors);
+	numActors = json["numActors"];
+	numProps = json["numProps"];
+	numScenes = json["numScenes"];
+	sceneData = json["sceneData"];
+	// TODO: make next and previous buttons appear
+
+	// defaults to displaying the first scene
+	displayScene(currScene);
+	$("#sceneNum").html(`Scene ${currScene}`);
 }
 
-$("#file").on("change", function() {
-	const file = $("#file").prop("files")[0];
+function displayScene(sceneNumber) {
+	const key = "Scene " + sceneNumber;
+	const currScene = sceneData[key];
+	// currScene["carryOff"]
+	// currScene["carryOn"]
+	// currScene["props"]
+	const actors = currScene["actors"];
+	actors.forEach(function(a) {
+		appendActor(a, "CENTER");
+	});
+
+	const actorsLeft = currScene["leftActors"];
+	actorsLeft.forEach(function(a) {
+		appendActor(a, "LEFT");
+	});
+
+	const actorsRight = currScene["rightActors"];
+	actorsRight.forEach(function(a) {
+		appendActor(a, "RIGHT");
+	});
+
+	// currScene["leftActors"]
+	// currScene["rightActors"]
+	// currScene["leftProps"]
+	// currScene["rightProps"]
+}
+
+function appendActor(name, position) {
+
+	selector = `#${position} .actorContainer`
+
+	const n = name.charAt(name.length - 1);
+
+	newElt = `
+	<div class="btn btn-warning btn-circle btn-lg">
+		<span>A${n}</span>
+	</div>`
+
+	$(selector).append(newElt);
+}
+
+
+$(document).ready(function() {
+
+	// instantiates a FileReader
 	const fr = new FileReader();
+
+	// sets the handler for when the filereader loads a file
 	fr.onload = function() {
-		console.log(fr.result);
+		// call the parse function above to parse the json
 		parse(fr.result);
 	};
-	fr.readAsText(file);
+
+	// adds a handler to the file input
+	$("#file").on("change", function() {
+
+		// gets the file that the user inputted
+		const file = $("#file").prop("files")[0];
+
+		// read the file (this triggers the onload handler above)
+		fr.readAsText(file);
+	});
+
+	$("#prevButton").on("click", function(){
+		if (currScene == 0) {
+			alert("There's no previous scene!");
+		} else {
+			$(".actorContainer").empty();
+			currScene = currScene - 1;
+			$("#sceneNum").html(`Scene ${currScene}`);
+			displayScene(currScene);
+		}
+	});
+
+	$("#nextButton").on("click", function(){
+
+		if (currScene == numScenes - 1) {
+			alert("There's no next scene!");
+		} else {
+			$(".actorContainer").empty();
+			currScene = currScene + 1;
+			$("#sceneNum").html(`Scene ${currScene}`);
+			displayScene(currScene);
+		}
+	});
 });
